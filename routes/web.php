1c6529f847719ab74f99ad05e47b6b044f8ab33d
Oxy Thu <oxythu27@gmail.com>
2021-05-29 11:14:25 +0630

    test route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','ArticleController@index');
+Route::get('/articles','ArticleController@index');
+Route::get('/articles/detail/{id}','ArticleController@detail');
